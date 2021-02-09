e3b77849928457cd16d8e5df611abcf6074d2c3f
errozz <errozz@gmail.com>
2021-02-09 23:37:29 +0500

    w2c2 minor update

--- w2c2_spendings_json/src/spendings_json.cpp
+++ w2c2_spendings_json/src/spendings_json.cpp
@@ -44,7 +44,7 @@ string MostExpensiveCategory(
 vector<Spending> LoadFromJson(istream& input) {
   Document doc = Load(input);
   vector<Spending> result;
-  for (const auto& node : doc.GetRoot().AsArray()) {
+  for (const Node& node : doc.GetRoot().AsArray()) {
     result.push_back({ node.AsMap().at("category").AsString(),
                        node.AsMap().at("amount").AsInt() });
   }
