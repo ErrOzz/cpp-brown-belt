1c40480854acdc065026c508b91c382d574e1adc
errozz <errozz@gmail.com>
2021-02-28 14:33:39 +0500

    w3c1 _print_stats_2 reference version
    Modifying to reference version

--- w3c1_print_stats_2/src/print_stats_2.cpp
+++ w3c1_print_stats_2/src/print_stats_2.cpp
@@ -78,35 +78,49 @@ const std::optional<string> MostPopularName(const IteratorRange<Iter>& range) {
   }
 }
 
-int main() {
-  vector<Person> people = ReadPeople(cin);
-  IteratorRange<vector<Person>::iterator> range{people.begin(),
-    partition(people.begin(), people.end(), [](const Person& p) {
-      return p.is_male;
-    })
-  };
-  const std::optional<string> most_pop_name_M = MostPopularName(range);
-  range = IteratorRange<vector<Person>::iterator>{range.end(), people.end()};
-  const std::optional<string> most_pop_name_F = MostPopularName(range);
+struct StatsData {
+  std::optional<string> most_popular_M_name;
+  std::optional<string> most_popular_F_name;
+  vector<int> cumulative_wealth;
+  vector<Person> age_sorted;
+};
 
-  const vector<Person> age_sorted = [&people]{
-    sort(begin(people), end(people), [](const Person& lhs, const Person& rhs) {
-      return lhs.age < rhs.age;
+StatsData BuildSatatsData(vector<Person> people) {
+  StatsData result;
+  {
+    IteratorRange<vector<Person>::iterator> range{people.begin(),
+      partition(people.begin(), people.end(), [](const Person& p) {
+        return p.is_male;
+      })
+    };
+    result.most_popular_M_name = MostPopularName(range);
+    range = IteratorRange<vector<Person>::iterator>{range.end(), people.end()};
+    result.most_popular_F_name = MostPopularName(range);
+  }
+
+  {
+    sort(people.begin(), people.end(), [](const Person& lhs, const Person& rhs) {
+      return lhs.income > rhs.income;
     });
-    return move(people);
-  }();
-
-  const vector<int> wealthy_sorted = [&age_sorted] {
-    vector<int> result;
-    result.reserve(age_sorted.size());
-    for (const auto& p : age_sorted) {
-      result.push_back(p.income);
+    vector<int>& wealth = result.cumulative_wealth;
+    wealth.resize(people.size());
+    if (people.size()) {
+      wealth[0] = people[0].income;
+      for (size_t i = 1; i < people.size(); ++i) {
+        wealth[i] = wealth[i - 1] + people[i].income;
+      }
     }
-    sort(result.begin(), result.end(), [](int lhs, int rhs) {
-      return lhs > rhs;
-    });
-    return result;
-  }();
+  }
+
+  sort(begin(people), end(people), [](const Person& lhs, const Person& rhs) {
+    return lhs.age < rhs.age;
+  });
+  result.age_sorted = std::move(people);
+  return result;
+}
+
+int main() {
+  const StatsData stats = BuildSatatsData(ReadPeople(cin));
 
   for (string command; cin >> command; ) {
     if (command == "AGE") {
@@ -114,29 +128,32 @@ int main() {
       cin >> adult_age;
 
       auto adult_begin = lower_bound(
-        begin(age_sorted), end(age_sorted), adult_age, [](const Person& lhs, int age) {
+        begin(stats.age_sorted), end(stats.age_sorted),
+              adult_age, [](const Person& lhs, int age) {
           return lhs.age < age;
         }
       );
 
-      cout << "There are " << std::distance(adult_begin, end(age_sorted))
+      cout << "There are " << std::distance(adult_begin, end(stats.age_sorted))
            << " adult people for maturity age " << adult_age << '\n';
     } else if (command == "WEALTHY") {
       size_t count;
       cin >> count;
-      int total_income = accumulate(wealthy_sorted.begin(),
-        wealthy_sorted.begin() + min(count, wealthy_sorted.size()), 0);
-      cout << "Top-" << count << " people have total income " << total_income << '\n';
+
+      cout << "Top-" << count << " people have total income "
+           << stats.cumulative_wealth[count - 1] << '\n';
     } else if (command == "POPULAR_NAME") {
       char gender;
       cin >> gender;
 
-      const std::optional<string>& most_popular_name = gender == 'M' ? most_pop_name_M : most_pop_name_F;
-      if (!most_popular_name.has_value()) {
-        cout << "No people of gender " << gender << '\n';
-      } else {
+      const std::optional<string>& most_popular_name
+        = gender == 'M' ? stats.most_popular_M_name
+                        : stats.most_popular_F_name;
+      if (most_popular_name) {
         cout << "Most popular name among people of gender " << gender << " is "
              << *most_popular_name << '\n';
+      } else {
+        cout << "No people of gender " << gender << '\n';
       }
     }
   }
