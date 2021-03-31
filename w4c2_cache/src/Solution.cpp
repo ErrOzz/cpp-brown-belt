66b421824a7e9216d69ee2204430b908eac47ca3
errozz <errozz@gmail.com>
2021-03-31 23:26:11 +0500

    w4c2_cache update
    half-multithread version:)

--- w4c2_cache/src/Solution.cpp
+++ w4c2_cache/src/Solution.cpp
@@ -14,13 +14,14 @@ struct BookAttr {
 
 class LruCache : public ICache {
   unordered_map<string, BookAttr> data_;
-  shared_ptr<IBooksUnpacker> unpacker_;
-  Settings settings_;
+  const shared_ptr<IBooksUnpacker> unpacker_;
+  const Settings settings_;
   size_t cur_memory = 0;
   size_t max_rating = 0;
-//  mutex mtx;
+  unique_ptr<mutex> mutex_ptr = make_unique<mutex>();
 
   void FreeMemory(size_t size) {
+    lock_guard<mutex> lock(*mutex_ptr);
     while (settings_.max_memory - cur_memory < size && cur_memory > 0) {
       auto min_it = min_element(data_.begin(), data_.end(),
                                 [](const auto& lhs, const auto& rhs){
@@ -41,7 +42,7 @@ public:
   BookPtr GetBook(const string& book_name) override {
     if (const auto it = data_.find(book_name); it != data_.end()) {
       {
-//        lock_guard<mutex> lock(mtx);
+        lock_guard<mutex> lock(*mutex_ptr);
         it->second.rating = ++max_rating;
       }
       return it->second.ptr;
@@ -50,8 +51,11 @@ public:
     auto new_book_size = new_book->GetContent().size();
     FreeMemory(new_book_size);
     if (new_book_size > settings_.max_memory) return move(new_book);
-    cur_memory += new_book_size;
-    data_[book_name] = {move(new_book), ++max_rating};
+    {
+      lock_guard<mutex> lock(*mutex_ptr);
+      cur_memory += new_book_size;
+      data_[book_name] = {move(new_book), ++max_rating};
+    }
     return data_[book_name].ptr;
   }
 };
