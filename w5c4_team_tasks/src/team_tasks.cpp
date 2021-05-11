8ea6f197e35479fb00a350240fb8fd59922f504f
errozz <errozz@gmail.com>
2021-05-11 23:22:58 +0500

    w5c4_team_tasks in progress

--- w5c4_team_tasks/src/team_tasks.cpp
+++ w5c4_team_tasks/src/team_tasks.cpp
@@ -1,4 +1,5 @@
 #include <map>
+#include <unordered_map>
 #include <tuple>
 
 // #define FOR_SEND
@@ -17,20 +18,45 @@ enum class TaskStatus {
 // Объявляем тип-синоним для map<TaskStatus, int>,
 // позволяющего хранить количество задач каждого статуса
 using TasksInfo = map<TaskStatus, int>;
+
 #endif
 
 class TeamTasks {
+  unordered_map<string, TasksInfo> person_tasks;
 public:
   // Получить статистику по статусам задач конкретного разработчика
-  const TasksInfo& GetPersonTasksInfo(const string& person) const;
+  const TasksInfo& GetPersonTasksInfo(const string& person) const {
+    return person_tasks.at(person);
+  }
 
   // Добавить новую задачу (в статусе NEW) для конкретного разработчитка
-  void AddNewTask(const string& person);
+  void AddNewTask(const string& person) {
+    ++person_tasks[person][TaskStatus::NEW];
+  }
 
   // Обновить статусы по данному количеству задач конкретного разработчика,
   // подробности см. ниже
   tuple<TasksInfo, TasksInfo> PerformPersonTasks(
-      const string& person, int task_count);
+      const string& person, int task_count) {
+    TasksInfo& tasks = person_tasks.at(person);
+    TasksInfo performed;
+    for (auto& [status, count] : tasks) {
+      if (task_count < 1 || status == TaskStatus::DONE) break;
+      auto shift_count = min(count, task_count);
+      performed[++static_cast<int>(status)] = shift_count;
+      // // Здесь может получиться нулевое количество задач
+      // if (count -= shift_count; count) {
+      //   rest[status] = count;
+      // }
+      task_count -= shift_count;
+    }
+    TasksInfo rest;
+    for (int status = 0; status < 4; ++status) {
+      int count = tasks.count(status) ? tasks[status] : 0;
+      count -= performed.count(status + 1) ? performed[status + 1] : 0;
+      if (count) rest[status] = count;
+    }
+  }
 };
 
 // Принимаем словарь по значению, чтобы иметь возможность
