1ee5e006fe47cb97f2b86d1e58b79067b6915ed1
errozz <errozz@gmail.com>
2021-06-08 00:54:46 +0500

    w5c5_booking starting

--- w5c5_booking/src/booking.cpp
+++ w5c5_booking/src/booking.cpp
@@ -4,11 +4,25 @@
 
 using namespace std;
 
+class HotelManager {
+
+public:
+  void Book(int clien_id, int room_count, long long int time) {
+    
+  }
+  int Clients() {
+
+  }
+  int Rooms() {
+
+  }
+};
+
 int int main() {
   int q;
   cin >> q;
   do {
-    
+
   } while (--q > 0);
   return 0;
 }
