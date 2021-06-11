b565c0d7958e1d87a594ed457118c4447dc92785
errozz <errozz@gmail.com>
2021-06-11 23:20:22 +0500

    w5c5_booking approved version

--- w5c5_booking/src/booking.cpp
+++ w5c5_booking/src/booking.cpp
@@ -1,4 +1,4 @@
-#include "test_runner.h"
+//#include "test_runner.h"
 
 #include <iostream>
 #include <list>
@@ -20,53 +20,51 @@ class HotelManager {
   int client_count = 0;
   int room_count = 0;
 
-  static const int day_duration = 86400;
+  static const int DAY_DURATION = 86400;
 
-  void RemoveOldBookings() {
-    long long int current_time = day_que.back().time;
-    for (auto& front_book = day_que.front();
-           front_book.time + day_duration  <= current_time;) {
-      auto client_it = client_appears.find(front_book.client_id);
+  void RemoveOldBookings(const long long int& current_time) {
+    while (day_que.size() &&
+           day_que.front().time + DAY_DURATION  <= current_time) {
+      auto client_it = client_appears.find(day_que.front().client_id);
       if (--client_it->second == 0) {
         client_appears.erase(client_it);
         --client_count;
       }
-      room_count -= front_book.room_count;
+      room_count -= day_que.front().room_count;
       day_que.pop_front();
     }
   }
 public:
-  void Book(long long int time, int client_id, int rooms) {
-    day_que.push_back({time, client_id, rooms});
-    ++client_appears[client_id];
-    if (client_appears[client_id] == 1) ++client_count;
-    room_count += rooms;
-    RemoveOldBookings();
+  void Book(Book_Item book) {
+    day_que.push_back(move(book));
+    ++client_appears[book.client_id];
+    if (client_appears[book.client_id] == 1) ++client_count;
+    room_count += book.room_count;
   }
-  int Clients() const {
+  int Clients(const long long int& current_time) {
+    RemoveOldBookings(current_time);
     return client_count;
   }
-  int Rooms() const {
+  int Rooms(const long long int& current_time) {
+    RemoveOldBookings(current_time);
     return room_count;
   }
 };
 
 class BookManager {
   unordered_map<string, HotelManager> bookings;
+  long long int current_time;
 public:
   void Book(long long int time, string hotel_name,
             int client_id, int rooms) {
-    bookings[move(hotel_name)].Book(time, client_id, rooms);
+    bookings[move(hotel_name)].Book({time, client_id, rooms});
+    current_time = time;
   }
-  int Clients(const string& hotel_name) const {
-    auto it = bookings.find(hotel_name);
-    if (it == bookings.end()) return 0;
-    return it->second.Clients();
+  int Clients(const string& hotel_name) {
+    return bookings[hotel_name].Clients(current_time);
   }
-  int Rooms(const string& hotel_name) const {
-    auto it = bookings.find(hotel_name);
-    if (it == bookings.end()) return 0;
-    return it->second.Rooms();
+  int Rooms(const string& hotel_name) {
+    return bookings[hotel_name].Rooms(current_time);
   }
 };
 
